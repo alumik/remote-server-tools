7af9b48d6d3899cbd39ff7411edf5a0f42e3fafa
Zhong Zhenyu <nczzy1997@gmail.com>
2023-03-05 17:34:34 +0800

    Separate data and code

--- gpu-guard/gpu-guard.py
+++ gpu-guard/gpu-guard.py
@@ -1,17 +1,15 @@
 import re
-import os
 import sys
 import json
 import logging
+import pathlib
 import requests
 import subprocess
 
 from logging.handlers import RotatingFileHandler
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(script_dir, 'whitelist.json')
-with open(file_path, 'r') as f:
-    whitelist = json.load(f)
+with open(pathlib.Path(__file__).resolve().parent / 'config.json', 'r') as f:
+    config = json.load(f)
 
 pids = subprocess.run(
     ['nvidia-smi', '--query-compute-apps=pid', '--format=csv,noheader'],
@@ -21,15 +19,16 @@ pids = [int(pid) for pid in pids.strip().split()]
 pids = set(pids)
 
 server = sys.argv[1]
-job_pids = requests.get('http://10.10.1.210/api/v1/job/gpu?server=' + server, timeout=5)
-job_pids.raise_for_status()
-job_pids = set(json.loads(job_pids.content))
+registered_pids = requests.get(config['urls']['pid_query'].replace('{server}', server), timeout=5)
+registered_pids.raise_for_status()
+registered_pids = set(json.loads(registered_pids.content))
 
-unregistered_pids = pids - job_pids
+unregistered_pids = pids - registered_pids
 
 log_formatter = logging.Formatter('[%(asctime)s] %(message)s')
+pathlib.Path(config['log']).parent.mkdir(parents=True, exist_ok=True)
 my_handler = RotatingFileHandler(
-    '/root/gpu-guard.log',
+    config['log'],
     mode='a',
     maxBytes=100 * 1024 * 1024,
     backupCount=2,
@@ -44,7 +43,7 @@ app_log.addHandler(my_handler)
 
 print(json.dumps({
     'pids': list(pids),
-    'job_pids': list(job_pids),
+    'registered_pids': list(registered_pids),
     'unregistered_pids': list(unregistered_pids),
 }, indent=2))
 
@@ -56,9 +55,9 @@ for pid in unregistered_pids:
         ).stdout.decode('utf-8').strip()
         app_log.info(process_info)
         _pid, user, command = [item.strip() for item in re.sub(r'\s+', ' ', process_info).split(maxsplit=2)]
-        if user not in whitelist.get('users', []):
+        if user not in config['whitelist']['users']:
             requests.post(
-                'http://10.10.1.210/api/v1/server/killed',
+                config['urls']['register_kill'],
                 json={'server': server, 'pid': _pid, 'user': user, 'command': command},
                 timeout=5,
             )
