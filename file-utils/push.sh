f3b0ed34b921825b112c052b17041b0fa02b24b4
Zhong Zhenyu <nczzy1997@gmail.com>
2023-06-28 23:47:20 +0800

    Load server list from the correct path

--- file-utils/push.sh
+++ file-utils/push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+BASEDIR=$(dirname "$0")
 LOCAL=$1
 REMOTE=$2
 PASSWORD_PREFIX=$3
@@ -8,4 +9,4 @@ while read -r host ip; do
   echo Pushing to server "$host"
   scp -r "$LOCAL" server-admin@"$ip":~/payload
   echo "$PASSWORD_PREFIX""$host" | ssh -tt server-admin@"$ip" "sudo mv ~/payload $REMOTE"
-done <servers.txt
+done <"$BASEDIR"/servers.txt
