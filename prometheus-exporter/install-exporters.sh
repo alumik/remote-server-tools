b0be4d7b12f14e7f9e7076be36bc4fc1f31905b5
Zhong Zhenyu <nczzy1997@gmail.com>
2023-07-11 21:57:10 +0800

    Update exporter installing script

--- prometheus-exporter/install-exporters.sh
+++ prometheus-exporter/install-exporters.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+echo "Installing node exporter"
+service node-exporter stop
 cp node-exporter /usr/local/bin
-cp nvidia-gpu-exporter /usr/local/bin
 chmod +x /usr/local/bin/node-exporter
-chmod +x /usr/local/bin/nvidia-gpu-exporter
 cp node-exporter.service /etc/systemd/system/
-cp nvidia-gpu-exporter.service /etc/systemd/system/
-
 systemctl enable --now node-exporter
-systemctl enable --now nvidia-gpu-exporter
+
+if [ "${@: -1}" != "--cpu-only" ]; then
+  echo "Installing NVIDIA GPU exporter"
+  service nvidia-gpu-exporter stop
+  cp nvidia-gpu-exporter /usr/local/bin
+  chmod +x /usr/local/bin/nvidia-gpu-exporter
+  cp nvidia-gpu-exporter.service /etc/systemd/system/
+  systemctl enable --now nvidia-gpu-exporter
+fi
